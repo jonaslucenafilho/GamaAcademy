91e0a313d30e275987b58abd1881d2c595f98c3d
jonaslucenafilho <jonaslucena@outlook.com>
2021-10-09 15:06:26 -0300

    subindo exercício de tipos primitivos

---
+++ Exercício Tipos Primitivos/src/br/com/tipos/primitivos/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package br.com.tipos.primitivos;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		byte tipoByte = 123;
+		short tipoShort = 12345;
+		char tipoChar = 'a';
+		float tipoFloat = 2.1f;
+		double tipoDouble = 1.52;
+		int tipoInt = 5564216;
+		long tipoLong = 1598410525l;
+		boolean tipoBoolean = true;
+		
+		System.out.println("Valor do tipoByte = " + tipoByte +
+				"\nValor do tipoShort = " + tipoShort +
+				"\nValor do tipoChar = " + tipoChar +
+				"\nValor do tipoFloat = " + tipoFloat +
+				"\nValor do tipoDouble = " + tipoDouble +
+				"\nValor do tipoInt = " + tipoInt +
+				"\nValor do tipoLong = " + tipoLong +
+				"\nValor do tipoBoolean = " + tipoBoolean);	
+	}
+}
